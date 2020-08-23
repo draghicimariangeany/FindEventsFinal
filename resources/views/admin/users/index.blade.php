59556d4e608d3fd6645524e602d9ccbcc37eaa89
Geany <draghicimariangeany@gmail.com>
2020-08-23 08:26:26 -0700

    i added roles migration with seed and view users table

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -8,11 +8,32 @@
                     <div class="card-header">Users</div>
 
                     <div class="card-body">
-                        @foreach($users as $user)
 
-                            {{$user->name}} - {{$user->email}}
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th scope="col">Id</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($users as $user)
+                                    <tr>
+                                        <th scope="row">{{$user->id}}</th>
+                                        <td>{{$user->name}}</td>
+                                        <td>{{$user->email}}</td>
+                                        <td>
+                                            <a href="{{route('admin.users.edit', $user->id)}}"><button class="btn btn-primary">Edit</button></a>
+                                            <a href="{{route('admin.users.destroy', $user->id)}}"> <button class="btn btn-warning">Delete</button></a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
 
-                        @endforeach
+
                     </div>
                 </div>
             </div>
